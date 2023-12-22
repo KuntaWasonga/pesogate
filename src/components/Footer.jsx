94b15020dde3eeabb9cf001acf039a4e913c53cd
KuntaWasonga <wkunta@rocketmail.com>
2023-12-22 12:31:54 +0300

    Modified footer details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,11 +26,10 @@ const Footer = () => (
               {footerlink.links.map((link, index) => (
                 <li
                   key={link.name}
-                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
-                    index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
-                  }`}
-                >
-                  {link.name}
+                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer
+                  ${ index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"}`
+                            }>
+                  <a href={link.link}>{link.name}</a>
                 </li>
               ))}
             </ul>
